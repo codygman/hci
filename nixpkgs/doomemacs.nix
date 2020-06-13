c7cc4c95f0ee52d346bc0ec2dc16b57657c08c7d
codygman <cody@codygman.dev>
2020-06-13 07:56:31 +0000

    latest working commit

--- nixpkgs/doomemacs.nix
+++ nixpkgs/doomemacs.nix
@@ -1,7 +1,7 @@
 let
   emacs-overlay = builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/52b9fd468cd45f85c43f9b623ed2854971d8e1ad.tar.gz";
   pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
-  nix-doom-emacs = builtins.fetchTarball "https://github.com/vlaci/nix-doom-emacs/archive/b5d3de560dc0dfa1d517700d94344959d0fa1f61.tar.gz";
+  nix-doom-emacs = builtins.fetchTarball "https://github.com/vlaci/nix-doom-emacs/archive/303b511350e013f7b579c396e2a98e9174f30d22.tar.gz";
 in
   with pkgs; callPackage nix-doom-emacs {
     bundledPackages = false;
