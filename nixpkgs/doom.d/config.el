e804bbee051b2070807534acd7b52f08f70d018e
codygman <cody@codygman.dev>
2020-06-15 00:39:44 -0500

    add lsp stuff

--- nixpkgs/doom.d/config.el
+++ nixpkgs/doom.d/config.el
@@ -3,3 +3,9 @@
       evil-escape-key-sequence "jf"
       doom-font (font-spec :family "Source Code Pro" :size 17)
       )
+
+
+(setq
+ flycheck-disabled-checkers '(haskell-stack-ghc haskell-ghc) ;; only allow lsp checker
+ lsp-enable-file-watchers t ;; watch files for changes (replace ghcid functionality basically)
+ )
