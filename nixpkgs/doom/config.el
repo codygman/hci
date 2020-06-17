849ee1f70d0346e1d57c6b3216985680aada874c
codygman <cody@itpro.tv>
2020-06-16 23:43:37 -0500

    hotfix lsp ignores not seeming to match
    https://github.com/emacs-lsp/lsp-mode/issues/1815

--- nixpkgs/doom/config.el
+++ nixpkgs/doom/config.el
@@ -8,4 +8,38 @@
 (setq
  flycheck-disabled-checkers '(haskell-stack-ghc haskell-ghc) ;; only allow lsp checker
  lsp-enable-file-watchers t ;; watch files for changes (replace ghcid functionality basically)
- )
+
+ ;;  see https://github.com/emacs-lsp/lsp-mode/issues/1815
+ lsp-file-watch-threshold 2000          ;; This is okay hopefully?
+ lsp-file-watch-ignored
+ '("[/\\\\]\\.git"
+   "[/\\\\]\\.hg"
+   "[/\\\\]\\.bzr"
+   "[/\\\\]_darcs"
+   "[/\\\\]\\.svn"
+   "[/\\\\]_FOSSIL_"
+   "[/\\\\]\\.idea"
+   "[/\\\\]\\.ensime_cache"
+   "[/\\\\]\\.eunit"
+   "[/\\\\]node_modules"
+   "[/\\\\]\\.fslckout"
+   "[/\\\\]\\.tox"
+   "[/\\\\]\\.stack-work"
+   "[/\\\\]\\.bloop"
+   "[/\\\\]\\.metals"
+   "[/\\\\]target"
+   "[/\\\\]\\.ccls-cache"
+   "[/\\\\]\\.deps"
+   "[/\\\\]build-aux"
+   "[/\\\\]autom4te.cache"
+   "[/\\\\]\\.reference"
+
+   "[/\\\\]\\.docker"
+   "[/\\\\]dist-newstyle"
+   "[/\\\\]dist"
+   "[/\\\\]Documentation"
+   "[/\\\\]containers"
+   "[/\\\\]tf"
+   "[/\\\\]\\.semaphore"
+   "[/\\\\].*swp$"
+   "\./nix/materialized"))
