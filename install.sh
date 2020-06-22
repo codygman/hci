0006880bfa71d1a0b810a997176fdf935b11efaf
codygman <cody@codygman.dev>
2020-06-21 22:13:56 -0500

    try sourcing nix daemon
    I got this from ci logs seeing how nix action installs nix:
    
    https://github.com/codygman/hci/runs/793805224?check_suite_focus=true#step:3:159

--- install.sh
+++ install.sh
@@ -2,19 +2,11 @@
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
-echo "home looks like this"
-ls -larth "$HOME"
-
-echo "find nix.sh in .nix-profile"
-find "$HOME/.nix-profile" -iname "nix.sh"
-
-echo "actually what's in ~/.nix-profile?"
-find "$HOME/.nix-profile"
-
-echo "NIX_PATH: $NIX_PATH"
-export PATH=$HOME/.nix-profile/bin:$PATH
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  echo "sourcing nix daemon"
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
