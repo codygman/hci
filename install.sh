0d5e64088b97418e0c1959f9ac76ed88a69a420d
codygman <cody@codygman.dev>
2020-06-21 19:28:54 -0500

    upgrade nix

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+nix upgrade-nix
+
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
