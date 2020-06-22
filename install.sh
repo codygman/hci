956682486ed73eee6b4d91d5dd43b73713c70ac3
codygman <cody@codygman.dev>
2020-06-21 19:38:42 -0500

    try github actions

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-nix upgrade-nix
+# nix upgrade-nix
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
-nix-env -iA cachix -f https://cachix.org/api/v1/install
+# export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+# nix-env -iA cachix -f https://cachix.org/api/v1/install
 
-echo "configure machine to use cachix"
-cachix use codygman5
+# echo "configure machine to use cachix"
+# cachix use codygman5
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
 rm -v ~/.bashrc
 rm -v ~/.zshrc
+home-manager switch
 
 # install home-manager
 # ./bootstrap.hs
