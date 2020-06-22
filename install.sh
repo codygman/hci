80a32882b698ba1705562990cd4ab643626ff582
codygman <cody@codygman.dev>
2020-06-21 22:27:58 -0500

    copy home-manager config

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+# export PATH=$HOME/.nix-profile/bin:$PATH
+cp -Rv "$HOME/hci/nixpkgs" "$HOME/.config/nixpkgs"
 nix-shell '<home-manager>' -A install
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 echo "nix profile bin"
 ls $HOME/.nix-profile/bin
-export PATH=$HOME/.nix-profile/bin:$PATH
+
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
