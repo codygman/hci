3649a2e33cca2c50fde2f7978655dabfd31dc2d3
codygman <cody@codygman.dev>
2020-06-21 20:11:54 -0500

    source nix profile

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
