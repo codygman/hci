cf7fd7361179cfb822bc7424d1340362482aa053
codygman <cody@codygman.dev>
2020-06-21 22:01:55 -0500

    fix bash issues

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# nix upgrade-nix
+#! /usr/bin/env bash
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
@@ -7,7 +7,7 @@ export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
-[[ -f ~/.bashrc ]] | rm -v ~/.bashrc
-[[ -f ~/.zshrc ]] | rm -v ~/.zshrc
+[[ -f ~/.bashrc ]] || rm -v ~/.bashrc
+[[ -f ~/.zshrc ]] || rm -v ~/.zshrc
 home-manager switch
 emacs -batch -f buttercup-run-discover
