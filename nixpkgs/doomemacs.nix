19c2a9de2f3ad9cebea64c9040ea94b6b51b51cd
codygman <cody@codygman.dev>
2020-06-21 21:53:23 -0500

    remove unused

--- nixpkgs/doomemacs.nix
+++
@@ -1,9 +0,0 @@
-{ sources, pkgs }:
-let
-  nix-doom-emacs = import sources.nix-doom-emacs;
-in
-  with pkgs; callPackage nix-doom-emacs {
-    bundledPackages = false;
-    emacsPackages = emacsPackagesFor emacsGit;
-    doomPrivateDir = ./doom;
-  }
