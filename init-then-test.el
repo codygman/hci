4ee1b95aaa1f786b7be62466e6fb8f36f9d824b6
codygman <cody@codygman.dev>
2020-06-22 22:59:51 -0500

    debug directory we're in; use absolute path
    abs path is okay since this is a test, but ideally we wouldn't need to

--- init-then-test.el
+++ init-then-test.el
@@ -1,2 +1,4 @@
-(load "init.el")
+(message (format "we are in %s" (pwd)))
+(load (format "%s/init.el" (getenv "TRAVIS_BUILD_DIR")))
+(message (format "now we are in %s" (pwd)))
 (buttercup-run-discover)
