cea0fbfa399cbd54e59e13b9db7f62b651c018cf
codygman <cody@codygman.dev>
2020-06-22 19:13:53 -0500

    remove bash before home-manager install

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ ln -rs ~/hci/nixpkgs ~/.config/nixpkgs
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+# remove zshrc and bashrc so home-manager can overwrite them
+# TODO add this into bootstrap.hs and only do this on travis?
+rm -v ~/.bashrc
+rm -v ~/.zshrc
 nix-shell '<home-manager>' -A install
 
 [ -x "$(command -v home-manager)" ] || echo "home-manager failed to install"; exit 1;
@@ -21,12 +25,3 @@ nix-shell '<home-manager>' -A install
 echo "configure machine to use cachix"
 cachix use codygman5
 
-
-
-# remove zshrc and bashrc so home-manager can overwrite them
-# TODO add this into bootstrap.hs and only do this on travis?
-rm -v ~/.bashrc
-rm -v ~/.zshrc
-
-# install home-manager
-./bootstrap.hs
