e49b2100a872606f349d4c616a4349c639e466bb
codygman <cody@codygman.dev>
2020-06-22 19:01:50 -0500

    add travis back

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
-#! /usr/bin/env bash
+#!/usr/bin/env sh
+
+set -o errexit
+
+ln -rs ~/hci/nixpkgs ~/.config/nixpkgs
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
-export PATH=$HOME/.nix-profile/bin:$PATH
-cp -Rv "$HOME/hci/nixpkgs" "$HOME/.config/nixpkgs"
 nix-shell '<home-manager>' -A install
-. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-echo "nix profile bin"
-ls $HOME/.nix-profile/bin
+
+[ -x "$(command -v home-manager)" ] || echo "home-manager failed to install"; exit 1;
+[ -x "$(command -v cachix)" ] || echo "cachix failed to install"; exit 1;
+
+echo "configure machine to use cachix"
+cachix use codygman5
+
 
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
-[[ ! -f ~/.bashrc ]] || rm -v ~/.bashrc
-[[ ! -f ~/.zshrc ]] || rm -v ~/.zshrc
-home-manager switch
-emacs -batch -f buttercup-run-discover
+rm -v ~/.bashrc
+rm -v ~/.zshrc
+
+# install home-manager
+./bootstrap.hs
