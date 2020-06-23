c33ebcc2bdde16613936e3cff64ad86ef9701941
codygman <cody@codygman.dev>
2020-06-22 23:48:33 -0500

    add back tests-run

--- my-tests.el
+++ my-tests.el
@@ -1,28 +1,29 @@
+;; TODO checkout ert-expectations https://github.com/emacsorphanage/ert-expectations/blob/aed70e002c4305b66aed7f6d0d48e9addd2dc1e6/ert-expectations.el#L68
+(defun tests-run ()
+  "Run Evil tests."
+  (interactive '(nil t))
+  ;; We would like to use `ert-run-tests-batch-and-exit'
+  ;; Unfortunately it doesn't work outside of batch mode, and we
+  ;; can't use batch mode because we have tests that need windows.
+  ;; Instead, run the tests interactively, copy the results to a
+  ;; text file, and then exit with an appropriate code.
+  (setq attempt-stack-overflow-recovery nil
+	attempt-orderly-shutdown-on-fatal-signal nil)
+  (unwind-protect
+      (progn
+	(ert-run-tests-interactively t)
+	(with-current-buffer "*ert*"
+	  (append-to-file (point-min) (point-max) "test-results.txt")
+	  (when (not (getenv "DEBUG_TESTS")) (kill-emacs (if (zerop (ert-stats-completed-unexpected ert--results-stats)) 0 1))))
+    (unwind-protect
+	(progn
+	  (append-to-file "Error running tests\n" nil "test-results.txt")
+	  (append-to-file (backtrace-to-string (backtrace-get-frames 'backtrace)) nil "test-results.txt"))
+      (when (not (getenv "DEBUG_TESTS")) (kill-emacs 2))))))
+
 (ert-deftest true-is-true ()
   (should t))
 
-;; TODO checkout ert-expectations https://github.com/emacsorphanage/ert-expectations/blob/aed70e002c4305b66aed7f6d0d48e9addd2dc1e6/ert-expectations.el#L68
-;; (defun tests-run ()
-;;   "Run Evil tests."
-;;   (interactive '(nil t))
-;;   ;; We would like to use `ert-run-tests-batch-and-exit'
-;;   ;; Unfortunately it doesn't work outside of batch mode, and we
-;;   ;; can't use batch mode because we have tests that need windows.
-;;   ;; Instead, run the tests interactively, copy the results to a
-;;   ;; text file, and then exit with an appropriate code.
-;;   (setq attempt-stack-overflow-recovery nil
-;; 	attempt-orderly-shutdown-on-fatal-signal nil)
-;;   (unwind-protect
-;;       (progn
-;; 	(ert-run-tests-interactively t)
-;; 	(with-current-buffer "*ert*"
-;; 	  (append-to-file (point-min) (point-max) "test-results.txt")
-;; 	  (when (not (getenv "DEBUG_TESTS")) (kill-emacs (if (zerop (ert-stats-completed-unexpected ert--results-stats)) 0 1))))
-;;     (unwind-protect
-;; 	(progn
-;; 	  (append-to-file "Error running tests\n" nil "test-results.txt")
-;; 	  (append-to-file (backtrace-to-string (backtrace-get-frames 'backtrace)) nil "test-results.txt"))
-;;       (when (not (getenv "DEBUG_TESTS")) (kill-emacs 2))))))
 
 ;; duplicate in init.el
 ;; (defun emacs-d-directory ()
