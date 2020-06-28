1a8738bc809cbaf8dc0dbfe3d4e3be4ae3f8caf8
codygman <cody@codygman.dev>
2020-06-27 23:59:00 -0500

    emacs 28 via overlay working

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,38 +1,20 @@
-{ ... }:
+{ config, pkgs, ... }:
 let
   sources = import ./nix/sources.nix;
   emacs-overlay = sources.emacs-overlay;
-  pkgs = import sources.nixpkgs { overlays = [ (import emacs-overlay) ]; };
-  nivpkg = import sources.niv {};
-  home-manager = import sources.home-manager { pkgs = pkgs; };
   myEnv = builtins.getEnv "MYENV";
   lib = pkgs.lib;
-  extraConfigImports = if myEnv != ""
-            then if myEnv == "personal" then
-              lib.info "loading PERSONAL home manager environment"
-                [ ~/Sync/nix-home-manager-config/personal.nix ]
-                 else
-                   if myEnv == "work" then
-                     lib.info "loading WORK home manager environment"
-                       [ ~/Sync/nix-home-manager-config/work.nix ]
-                   else
-                     lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
-            else
-              lib.warn "MYENV not specified, ONLY core home environment will be available!" [];
 in
 {
+
+ nixpkgs = {
+    overlays = [
+       (import sources.emacs-overlay)
+    ];
+  };
   programs = {
     home-manager.enable = true;
-    git = {
-      enable = true;
-      userName = "codygman";
-      userEmail = lib.mkDefault "cody@codygman.dev";
-    };
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-    };
+
     emacs = {
       enable = true;
       # package = if builtins.getEnv "TRAVIS_OS_NAME" == "" then emacs-overlay.emacs else pkgs.emacs-nox;
@@ -44,6 +26,24 @@ in
         evil-collection
         magit
       ];
+    };
+
+
+
+
+
+
+
+
+    git = {
+      enable = true;
+      userName = "codygman";
+      userEmail = lib.mkDefault "cody@codygman.dev";
+    };
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
     };
     bash = {
       enable = true;
