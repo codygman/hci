fa10c4fe2ddf7cb7e967d6369ba9c2b704ea9108
codygman <cody@codygman.dev>
2020-06-28 01:17:32 -0500

    stack clean haskell test project before testing

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ fi
 # echo "after: \n$PATH"
 home-manager switch
 # echo "running emacs: $(which emacs)"
+# Before running tests, clean the stack directory for our haskell test project
+pushd testdata/simple-haskell-project && stack clean && popd
 EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
 emacsExitCode=$?;
 cat test-results.txt
