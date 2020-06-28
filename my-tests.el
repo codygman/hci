d163d15d515541cfa6728d13a1343b1214b4ed0f
codygman <cody@codygman.dev>
2020-06-28 01:19:19 -0500

    add ghci locals test back

--- my-tests.el
+++ my-tests.el
@@ -72,40 +72,40 @@
 ;; ;; TODO test for "jf" escape working
 ;; ;; TODO test for "Ctrl u" working
 
-;; (defun copy-line (arg)
-;;   "Copy lines (as many as prefix argument) in the kill ring"
-;;   ;; (interactive "p")
-;;   (kill-ring-save (line-beginning-position)
-;;                   (line-beginning-position (+ 1 arg)))
-;;   (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  ;; (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
 
-;; (require 'subr-x)
+(require 'subr-x)
 
-;; (defun my-append-string-to-file (s filename)
-;;   (with-temp-buffer
-;;     (insert s)
-;;     (write-region (point-min) (point-max) filename t)))
+(defun my-append-string-to-file (s filename)
+  (with-temp-buffer
+    (insert s)
+    (write-region (point-min) (point-max) filename t)))
 
-;; (defun wait-for-ghci ()
-;;   ;; TODO figure out how to maybe use haskell-mode-interactive-prompt-state
-;;   (sit-for 5))
+(defun wait-for-ghci ()
+  ;; TODO figure out how to maybe use haskell-mode-interactive-prompt-state
+  (sit-for 5))
 
-;; (ert-deftest ghci-has-locals-in-scope ()
-;;   (interactive)
-;;   (find-file (emacs-d-directory-for "testdata/simple-haskell-project/Main.hs"))
-;;   (my-append-string-to-file "START load-file\n" "debug")
-;;   (haskell-process-load-file)
-;;   (sit-for 5)
-;;   (with-current-buffer "*simple-haskell-project*"
-;;     (evil-append-line 1)
-;;     (insert ":t functionWeWantInScope")
-;;     (haskell-interactive-mode-return)
-;;     (sit-for 5)
-;;     (evil-previous-line 1)
-;;     (copy-line 1))
-;;   (should (string-equal
-;;            (string-trim (substring-no-properties (nth 0 kill-ring)))
-;;            "functionWeWantInScope :: ()")))
+(ert-deftest ghci-has-locals-in-scope ()
+  (interactive)
+  (find-file (emacs-d-directory-for "testdata/simple-haskell-project/Main.hs"))
+  (my-append-string-to-file "START load-file\n" "debug")
+  (haskell-process-load-file)
+  (sit-for 5)
+  (with-current-buffer "*simple-haskell-project*"
+    (evil-append-line 1)
+    (insert ":t functionWeWantInScope")
+    (haskell-interactive-mode-return)
+    (sit-for 5)
+    (evil-previous-line 1)
+    (copy-line 1))
+  (should (string-equal
+           (string-trim (substring-no-properties (nth 0 kill-ring)))
+           "functionWeWantInScope :: ()")))
 
 
 ;; TODO add ghcide (or hls or hie) and lsp, lsp-ui tests
