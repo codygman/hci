156cf6d762059637905eb51b4e40423d8ec465e9
codygman <cody@codygman.dev>
2020-06-27 22:46:44 -0500

    silence logs; remove test file after run

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
     . ~/.nix-profile/etc/profile.d/nix.sh
     export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 fi
-echo "after: \n$PATH"
+# echo "after: \n$PATH"
 home-manager switch
-echo "running emacs: $(which emacs)"
+# echo "running emacs: $(which emacs)"
 EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
 cat test-results.txt
+if [ -f "test-results.txt" ]; then
+    rm test-results.txt
+fi
