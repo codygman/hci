499b69cdd8e67df5ed4b0d6b988c2eab6ffdf9c7
codygman <cody@codygman.dev>
2020-06-28 15:44:02 -0500

    remove trailing slash

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 export PATH=$HOME/.nix-profile/bin:$PATH
 
-ln -sv "$TRAVIS_BUILD_DIR/nixpkgs" /home/runner/.config/nixpkgs/
+ln -sv "$TRAVIS_BUILD_DIR/nixpkgs" /home/runner/.config/nixpkgs
 
 echo ""
 echo ""
