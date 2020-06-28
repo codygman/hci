9f5e4571edb7775cf0bd32a9f239f92432f04c99
codygman <cody@codygman.dev>
2020-06-28 14:56:44 -0500

    ensure we're linking to hci nixpkgs correctly

--- install.sh
+++ install.sh
@@ -1,15 +1,37 @@
 #! /usr/bin/env bash
 
+set -o errexit
+
+function check_installed() {
+    echo "checking we have $1"
+    if [ -x "$(command -v $1)" ]; then
+	echo ""
+	echo "=================================================="
+        echo "$1 installed, moving on"
+	echo "=================================================="
+	echo ""
+    else
+        echo "$1 not installed or not in PATH";
+	exit 1;
+    fi
+}
+
+
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 export PATH=$HOME/.nix-profile/bin:$PATH
-cp -Rv "$HOME/hci/nixpkgs" "$HOME/.config/nixpkgs"
+
+ln -rs "$TRAVIS_BUILD_DIR/nixpkgs" ~/.config/nixpkgs
+echo "realpath of nixpkgs before install: $(realpath ~/.config/nixpkgs)"
 nix-shell '<home-manager>' -A install
+
+check_installed "emacs"
+
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 echo "nix profile bin"
 ls $HOME/.nix-profile/bin
-
 
 # remove zshrc and bashrc so home-manager can overwrite them
 # TODO add this into bootstrap.hs and only do this on travis?
