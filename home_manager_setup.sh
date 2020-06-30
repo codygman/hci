8d538bc62689a215b9dc60c46ea9e18de15a7fda
codygman <cody@codygman.dev>
2020-06-30 18:52:03 -0500

    remove source of non existent hm-session-vars

--- home_manager_setup.sh
+++ home_manager_setup.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-
-. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-echo "nix profile bin"
-ls $HOME/.nix-profile/bin
-
 export PATH=$HOME/.nix-profile/bin:$PATH
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
