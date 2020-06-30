6e59ad749313ec738ee0bdc7b4aa60d736061e39
codygman <cody@codygman.dev>
2020-06-30 18:55:48 -0500

    make sure we export NIX_PATH

--- home_manager_setup.sh
+++ home_manager_setup.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 export PATH=$HOME/.nix-profile/bin:$PATH
+export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
