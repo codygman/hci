e4fe98b161b88f8bf34c00a2ef12e297a20267e4
codygman <cody@codygman.dev>
2020-06-30 20:44:48 -0500

    better describe message; remove sample spec

--- test-feature.el
+++ test-feature.el
@@ -1,5 +1,3 @@
-(describe "A suite"
-  (it "contains a spec with an expectation"
-      (expect t :to-be t))
+(describe "Set a baseline with simple tests"
   (it "emacs version should be 28.0.50"
       (expect emacs-version :to-equal "28.0.50")))
