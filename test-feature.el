ab1af725433d2f935b1adc960fc1203cb1371f6b
codygman <cody@codygman.dev>
2020-06-30 21:13:09 -0500

    add Haskell integration test

--- test-feature.el
+++ test-feature.el
@@ -7,4 +7,13 @@
                (expect emacs-version :to-equal "28.0.50"))
            )
 
+ (describe "Haskell Integration"
+
+           (it "haskell mode is active after opening a haskell file"
+               (find-file (emacs-d-directory-for "testdata/simple-haskell-project/Main.hs"))
+               (expect 'haskell-mode :to-equal (derived-mode-p 'haskell-mode))
+               )
+
+           )
+
  )
