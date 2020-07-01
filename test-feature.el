30e33fc007cb65e3d2bb7c0c9940dd834431d186
codygman <cody@codygman.dev>
2020-06-30 21:10:19 -0500

    reformat tests

--- test-feature.el
+++ test-feature.el
@@ -1,3 +1,10 @@
-(describe "Set a baseline with simple tests"
-  (it "emacs version should be 28.0.50"
-      (expect emacs-version :to-equal "28.0.50")))
+(describe
+ "Codygman's hci"
+
+ (describe "Set a baseline with simple tests"
+
+           (it "emacs version should be 28.0.50"
+               (expect emacs-version :to-equal "28.0.50"))
+           )
+
+ )
