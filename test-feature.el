36af7c63b64b215f04cc997d2c281bc2d0af1fe8
codygman <cody@codygman.dev>
2020-06-30 19:36:13 -0500

    try to-equal for strings

--- test-feature.el
+++ test-feature.el
@@ -2,4 +2,4 @@
   (it "contains a spec with an expectation"
       (expect t :to-be t))
   (it "emacs version should be 28.0.50"
-      (expect emacs-version :to-be "28.0.50")))
+      (expect emacs-version :to-equal "28.0.50")))
