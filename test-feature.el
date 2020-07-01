12105e62f684f9d45f8738bd6864853debb668a4
codygman <cody@codygman.dev>
2020-06-30 19:23:39 -0500

    add emacs version test

--- test-feature.el
+++ test-feature.el
@@ -1,3 +1,5 @@
 (describe "A suite"
   (it "contains a spec with an expectation"
-    (expect t :to-be t)))
+      (expect t :to-be t))
+  (it "emacs version should be 28.0.50"
+      (expect emacs-version :to-be "28.0.50")))
