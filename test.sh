4151be85aa25b4da9b8fa9cc71a6a279e6ddc56b
codygman <cody@codygman.dev>
2020-07-02 18:11:01 -0500

    be more compatible in dumb terminals

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ if [ $buttercupExitCode -ne 0 ]; then
     exit $buttercupExitCode;
 fi
 
-if [ -z "$IN_GIT_HOOK" ]; then
+if [ -z "$IN_GIT_HOOK" ] && [ "$TERM" != "dumb" ]; then
    echo "finished running buttercup tests, running ert tests"
    EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
    echo "finished running ert tests"
@@ -30,7 +30,7 @@ if [ -f "test-results.txt" ]; then
     echo "ert test results:"
     cat test-results.txt
     rm -v test-results.txt
-else
+elif [ "$TERM" != "dumb" ]; then
     echo "test-results.txt not present, did ert tests run?"
     echo "IN_GIT_HOOK is $IN_GIT_HOOK"
 fi
