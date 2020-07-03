6bb1bae6cfae54fdeb5f15d93801b2ce6c3d2604
codygman <cody@codygman.dev>
2020-07-02 23:32:48 -0500

    work around evil :general not working; add which-key
    since :general binds don't work or work weirdly within evil's
    use-package block I just won't put them there for now

--- init.el
+++ init.el
@@ -1,13 +1,56 @@
-(use-package general)
 
-(use-package evil
-  :after general
-  :config
-  (evil-mode 1)
-  :general
-  )
+(require 'org)
+(require 'use-package)
 
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+;; duplicate in init.el
+(defun emacs-d-directory-for (path)
+  (if (eq nil (getenv "GITHUB_WORKSPACE"))
+      (format "~/.emacs.d/%s" path)
+    (let ((github-workspace (getenv "GITHUB_WORKSPACE")))
+      (progn (message (format "emacs-d-directory-for: GITHUB_WORKSPACE is %s" github-workspace)) (format "%s/%s" github-workspace path)))))
+(org-babel-load-file (emacs-d-directory-for "readme.org"))
+
+
+
+;; (use-package general
+;;   :init
+;;   (setq evil-want-keybinding nil
+;; 	evil-want-C-d-scroll t
+;; 	evil-want-C-u-scroll t
+;; 	evil-want-integration t
+;; 	)
+;;   :config
+;;   (general-create-definer my-leader-def
+;;     :prefix "C")
+;;   (my-leader-def
+;;     :states '(normal visual emacs motion)
+;;     :prefix "SPC"
+;;     :keymaps 'override
+;;     :non-normal-prefix "M-SPC"
+;;     "u"   '(universal-argument :which-key "Universal Argument")
+;;     "tf" '(toggle-frame-fullscreen :which-key "Toggle Fullscreen")
+;;     "wd" '(delete-window :which-key "Delete Window")
+;;     "wm"  '(toggle-maximize-buffer :which-key "maximize buffer")
+
+;;     "hf" '(describe-function :which-key "Describe Function")
+;;     "hk" '(describe-key :which-key "Describe Key")
+;;     "ha" '(apropos-command :which-key "Apropos Command")
+;;     )
+;;   )
+
+;; (use-package evil
+;;   :after general
+;;   :config
+;;   (evil-mode 1)
+;;   :general
+;;   (nmap
+;;     :prefix "SPC"
+;;     ;; TODO uncommenting any of these causes an error where evil isn't loaded
+;;     "oo"  emacs-version
+;;     )
+;;   )
+
+;; (use-package evil-collection
+;;   :after evil
+;;   :config
+;;   (evil-collection-init))
