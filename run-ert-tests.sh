2e1a7c58359752e6545b97099d834eeb67de82e8
codygman <cody@codygman.dev>
2020-07-02 19:19:10 -0500

    run ert tests in separate step on actions
    so we can see more accurate (granular) timings

---
+++ run-ert-tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ -z "$IN_GIT_HOOK" ] && [ -z "$INSIDE_EMACS" ]; then
+    echo "finished running buttercup tests, running ert tests"
+    EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
+    echo "finished running ert tests"
+else
+    echo "in git hook, skipping ert test since we don't have TTY"
+fi
+
+emacsExitCode=$?;
+echo "emacs exit code will be: $emacsExitCode"
+
+# TODO improve git commit hooks and track with git via https://medium.com/@anandmohit7/improving-development-workflow-using-git-hooks-8498f5aa3345
+
+if [ -f "test-results.txt" ]; then
+    echo "ert test results:"
+    cat test-results.txt
+    rm -v test-results.txt
+elif [ "$TERM" != "dumb" ]; then
+    echo "test-results.txt not present, did ert tests run?"
+    echo "IN_GIT_HOOK is $IN_GIT_HOOK"
+fi
+
+exit $emacsExitCode
--- test.sh
+++ test.sh
@@ -13,30 +13,11 @@ if [ $buttercupExitCode -ne 0 ]; then
     exit $buttercupExitCode;
 fi
 
-if [ -z "$IN_GIT_HOOK" ] && [ -z "$INSIDE_EMACS" ]; then
-   echo "finished running buttercup tests, running ert tests"
-   EMACSFOR="PERSONAL" emacs -nw --load load-init-then-run-ert.el
-   echo "finished running ert tests"
-else
-    echo "in git hook, skipping ert test since we don't have TTY"
-fi
-
-emacsExitCode=$?;
-echo "emacs exit code will be: $emacsExitCode"
-
-# TODO improve git commit hooks and track with git via https://medium.com/@anandmohit7/improving-development-workflow-using-git-hooks-8498f5aa3345
-
-if [ -f "test-results.txt" ]; then
-    echo "ert test results:"
-    cat test-results.txt
-    rm -v test-results.txt
-elif [ "$TERM" != "dumb" ]; then
-    echo "test-results.txt not present, did ert tests run?"
-    echo "IN_GIT_HOOK is $IN_GIT_HOOK"
-fi
-
-
+# if this is github actions, we want to run this in it's own block for more granular timing info
+# if not, we're running it locally and just want to run it
 if [ -z "$GITHUB_ACTIONS" ]; then
-    exit $emacsExitCode;
+    echo "We are running locally, running ert tests..."
+    bash run-ert-tests.sh
+else 
+    echo "in github action, ert tests will be run directly"
 fi
-
