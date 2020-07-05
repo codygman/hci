e712c4bf1ec0d7907eb8c2856b3f358cb78bc5a8
codygman <cody@codygman.dev>
2020-07-04 20:47:48 -0500

    print out ghc we have in nix shell

--- prebuild-simple-haskell-project-nix-shell.sh
+++ prebuild-simple-haskell-project-nix-shell.sh
@@ -8,3 +8,5 @@ echo "it's directory looks like"
 ls | head -n5
 nix-shell --pure --run "echo \"finished building nix shell for simple-haskell\""
 echo "out of nix-shell invocation for building nix-shell for simple-haskell"
+echo "verify we have the right ghc"
+nix-shell --pure --run "realpath $(which ghc)"
