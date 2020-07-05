b12a647ed2facf7e8d042ef0967437029761c8c7
codygman <cody@codygman.dev>
2020-07-04 21:47:00 -0500

    use shell.nix with ghci, stack, etc

--- testdata/simple-haskell-project/shell.nix
+++ testdata/simple-haskell-project/shell.nix
@@ -1,5 +1,25 @@
 # shell.nix
 let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
   hsPkgs = import ./default.nix {};
 in
-  hsPkgs.simple-haskell-project.components.all
+  hsPkgs.shellFor {
+
+    # Include only the *local* packages of your project.
+    packages = ps: with ps; [
+      simple-haskell-project
+    ];
+
+    # Some common tools can be added with the `tools` argument
+    tools = { cabal = "3.2.0.0"; hlint = "2.2.11"; ghcide = "0.2.0"; brittany = "0.12.1.1";};
+    # See overlays/tools.nix for more details
+
+    # Some you may need to get some other way.
+    buildInputs = with pkgs.haskellPackages;
+      [ stack ghcid pkgs.postgresql ];
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
