5d01065a5efbd8a516ae469e0e1c5a4eb21e3f7c
codygman <cody@codygman.dev>
2020-07-10 16:10:07 -0500

    add cachix.nix and cachix directory
    Since I'll no longer have /etc/nixos/configuration.nix and be using
    the configuration.nix in ~/hci I believe it'll look for cachix
    configuration in directory that is specified by NIXOS_CONFIG.
    
    For me that will soon be:
    
    NIXOS_CONFIG=/home/cody/hci

---
+++ cachix/itprotv-codygman-test.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://itprotv-codygman-test.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "itprotv-codygman-test.cachix.org-1:jrttaeBtI1SnNxFIe8T3RlMpqME2GZu7nQTZci8Thzk="
+    ];
+  };
+}
+    
