7bb10dfc20719f834d9af3bb3295e859cf79b9be
codygman <cody@codygman.dev>
2020-07-10 17:55:27 -0500

    make sure git hook doesn't run sudo nixos-rebuild
    just run home-manager switch instead for now

--- switch.sh
+++ switch.sh
@@ -2,13 +2,19 @@
 
 # TODO check that we're actually on nixos, otherwise throw an error
 
-# TODO update nixos to use home-manager module for this script to actually sync the state of the system
-# This will be useful to ensure both my system level and user (or home) level states are in sync
-if grep -q "NixOS" /etc/issue; then
-    echo "we are on nixos; calling nixos-rebuild and assuming home-manager module enabled"
-    # TODO avoid needing sudo here by using nix-build directly, NOTE subsequent calls will somehow need to use the result symlink it generates
-    NIXOS_CONFIG=$(pwd)/nixpkgs/configuration.nix sudo -E nixos-rebuild switch
+
+if [ -z "$IN_GIT_HOOK" ] && [ -z "$INSIDE_EMACS" ]; then
+    if grep -q "NixOS" /etc/issue; then
+	echo "we are on nixos; calling nixos-rebuild and assuming home-manager module enabled"
+	# TODO avoid needing sudo here by using nix-build directly, NOTE subsequent calls will somehow need to use the result symlink it generates
+	NIXOS_CONFIG=$(pwd)/nixpkgs/configuration.nix sudo -E nixos-rebuild switch
+    else
+	echo "we are not on nixos, calling home-manager switch"
+	home-manager switch
+    fi
 else
-    echo "we are not on nixos, calling home-manager switch"
+    # TODO when we move to nixos-rebuild build/update commands to use that env we don't have to use home-manager here
+    # and it will result in a more reproducible environment
+    echo "in git hook, just running home-manager switch"
     home-manager switch
 fi
