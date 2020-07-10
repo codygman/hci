fcb4ac6cba8d7963b5056af587309bfb35d5e635
codygman <cody@codygman.dev>
2020-07-10 17:00:27 -0500

    make sure we pass the env var we set to sudoer env
    ```
    $ MYVAR="hi" sudo env | grep MYVAR
    $ MYVAR="hi" sudo -E env | grep MYVAR
    MYVAR=hi
    ```

--- switch_nixos_and_home_manager.sh
+++ switch_nixos_and_home_manager.sh
@@ -4,4 +4,4 @@
 
 # TODO update nixos to use home-manager module for this script to actually sync the state of the system
 # This will be useful to ensure both my system level and user (or home) level states are in sync
-# NIXOS_CONFIG=$(pwd)/configuration.nix sudo nixos-rebuild switch
+NIXOS_CONFIG=$(pwd)/configuration.nix sudo -E nixos-rebuild switch
