8f950949ba4cf6321863a8a795417a85cb403c6a
codygman <cody@codygman.dev>
2020-07-12 21:37:30 -0500

    change default hostname

--- nixpkgs/configuration.nix
+++ nixpkgs/configuration.nix
@@ -18,7 +18,7 @@
   };
 
   networking = {
-    hostName = "razer"; # Define your hostname.
+    hostName = "hci"; # I should make this machine specific.. maybe.. but while it's not why not call it hci :)
     networkmanager.enable = true;
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     useDHCP = false;
