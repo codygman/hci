248c070e18b67e2b6dc636edb840e294cc5676e6
codygman <cody@codygman.dev>
2020-07-18 22:42:04 -0500

    update

--- nixpkgs/diagrams-builder.nix
+++ nixpkgs/diagrams-builder.nix
@@ -12,47 +12,52 @@
 
 # Code
 let pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/c71518e75bf067fb639d44264fdd8cf80f53d75a.tar.gz") {};
-    my-haskell-src-exts =
-      pkgs.haskellPackages.callHackageDirect 
-        { pkg = "haskell-src-exts";
-          ver = "1.22.0";
-          sha256 = "0jwp8vhk3ncfxprbmg0jx001g7mh1kyp16973mjkjqz0d60zarwi";
-        } {};
-    my-haskell-src-exts-simple =
-      pkgs.haskellPackages.callHackageDirect 
-        { pkg = "haskell-src-exts-simple";
-          ver = "1.22.0.0";
-          sha256 = "1ixx2bpc7g6lclzrdjrnyf026g581rwm0iji1mn1iv03yzl3y215";
-        } {};
-    diagrams-postscript = 
-      pkgs.haskellPackages.callHackageDirect 
-        { pkg = "diagrams-postscript";
-          ver = "1.4.1";
-          sha256 = "0174y4s6rx6cckkbhph22ybl96h00wjqqkzkrcni7ylxcvgf37bd";
-        } {};
-in pkgs.diagrams-builder.overrideAttrs( oldAttrs : {
-  haskell-src-exts = my-haskell-src-exts;
-  haskell-src-exts-simple = my-haskell-src-exts-simple;
-})
+    myHaskellPackages = pkgs.haskellPackages.override {
+      overrides = self: super: {
+        haskell-src-exts = self.callHackageDirect 
+          { pkg = "haskell-src-exts";
+            ver = "1.22.0";
+            sha256 = "0jwp8vhk3ncfxprbmg0jx001g7mh1kyp16973mjkjqz0d60zarwi";
+          } {};
+        haskell-src-exts-simple = 
+          self.callHackageDirect 
+            { pkg = "haskell-src-exts-simple";
+              ver = "1.22.0.0";
+              sha256 = "1ixx2bpc7g6lclzrdjrnyf026g581rwm0iji1mn1iv03yzl3y215";
+            } {};
+      };
+    };
+in myHaskellPackages.diagrams-builder
+
+  # in pkgs.diagrams-builder.override( {
+  #       diagrams-builder = myHaskellPackages.diagrams-builder;
+  # })
+  # in pkgs.diagrams-builder
+
+
+  # in pkgs.diagrams-builder.overrideAttrs( oldAttrs : rec {
+  #   haskell-src-exts = my-haskell-src-exts;
+  #   haskell-src-exts-simple = my-haskell-src-exts-simple;
+  # })
 
 
 
 
 
 
-# an attempt at https://github.com/NixOS/nixpkgs/issues/25887
-# in pkgs.diagrams-builder.overrideAttrs(old: {
-#   overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {});
-# })
+  # an attempt at https://github.com/NixOS/nixpkgs/issues/25887
+  # in pkgs.diagrams-builder.overrideAttrs(old: {
+  #   overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {});
+  # })
 
 
-# (pkgs.diagrams-builder.overrideAttrs
-#   (oldAttrs: {
-#     haskell-src-exts = my-haskell-src-exts;
-#     haskell-src-exts-simple = my-haskell-src-exts-simple;
-#     diagrams-builder = 
-#       pkgs.haskell.packages.ghc883.diagrams-builder.override( {
-#         haskell-src-exts = my-haskell-src-exts;
-#         haskell-src-exts-simple = my-haskell-src-exts-simple;
-#       });
-#   }))
+  # (pkgs.diagrams-builder.overrideAttrs
+  #   (oldAttrs: {
+  #     haskell-src-exts = my-haskell-src-exts;
+  #     haskell-src-exts-simple = my-haskell-src-exts-simple;
+  #     diagrams-builder = 
+  #       pkgs.haskell.packages.ghc883.diagrams-builder.override( {
+  #         haskell-src-exts = my-haskell-src-exts;
+  #         haskell-src-exts-simple = my-haskell-src-exts-simple;
+  #       });
+  #   }))
