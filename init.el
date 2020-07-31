63661e00e8165e8932b01f564e8920710544d34b
codygman <cody@codygman.dev>
2020-07-31 18:23:36 -0500

    move general to init.el
    Basically otherwise the load order doesn't add up... because evil
    needs to make sure integration is nil... then general is used for
    use-package...
    
     oh man

--- init.el
+++ init.el
@@ -1,4 +1,49 @@
 
+(setq evil-want-keybinding nil)
+(require 'evil)
+(use-package general
+	:init
+	(setq evil-want-keybinding nil
+	      evil-want-C-d-scroll t
+	      evil-want-C-u-scroll t
+	      evil-want-integration t
+	      )
+	:config
+	(general-auto-unbind-keys) ;; NOTE not sure this works?
+	(general-evil-setup t)
+	(general-imap "j"
+	  (general-key-dispatch 'self-insert-command
+	    :timeout 0.25
+	    ;; TODO make this work so jf writes the file when I enter normal mode
+	    ;; "j" '(my-write-then-normal-state)
+	    "f" 'evil-normal-state))
+	(general-create-definer my-leader-def
+	  :prefix "C")
+	(my-leader-def
+	  :states '(normal visual emacs motion)
+	  :prefix "SPC"
+	  :keymaps 'override
+	  :non-normal-prefix "M-SPC"
+	  "u"   '(universal-argument :which-key "Universal Argument")
+	  "tf" '(toggle-frame-fullscreen :which-key "Toggle Fullscreen")
+	  "wd" '(delete-window :which-key "Delete Window")
+	  "qq" '(save-buffers-kill-terminal :which-key "Quit Emacs")
+
+	  "hf" '(describe-function :which-key "Describe Function")
+	  "hk" '(describe-key :which-key "Describe Key")
+	  "ha" '(apropos-command :which-key "Apropos Command")
+
+	  ;; window
+	  "wm"  '(toggle-maximize-buffer :which-key "maximize buffer")
+	  "wh"  '(evil-window-left :which-key "move left")
+	  "wj"  '(evil-window-down :which-key "move down a window")
+	  "wk"  '(evil-window-up :which-key "move up a window")
+	  "wl"  '(evil-window-right :which-key "move right a window")
+	  "wv"  '(split-window-right :which-key "split right a window")
+	  "ws"  '(split-window-below :which-key "split bottom")
+	  )
+	)
+(require 'general)
 (require 'org)
 (require 'use-package)
 
