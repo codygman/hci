8ea1329c4ee2c715349c115e82e679126d9b5e2a
codygman <cody@codygman.dev>
2020-07-31 14:24:04 -0500

    higher timeout for ghci locals test

--- my-tests.el
+++ my-tests.el
@@ -152,7 +152,7 @@
   ;; (my-append-string-to-file "START load-file\n" "debug")
   (haskell-process-load-file)
   (with-current-buffer "*simple-haskell-project*"
-    (wait-for-ghci "initial load" "^1$" 15)
+    (wait-for-ghci "initial load" "^1$" 30) ;; TODO see if you can lower me later
     (evil-append-line 1)
     (insert ":t functionWeWantInScope")
     (haskell-interactive-mode-return)
