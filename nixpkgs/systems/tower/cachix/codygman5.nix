8b958a452799fa68168ce087b7ace19764ca7b86
codygman <cody@codygman.dev>
2020-08-14 15:09:09 -0500

    move tower configuration into hci

---
+++ nixpkgs/systems/tower/cachix/codygman5.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://codygman5.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "codygman5.cachix.org-1:pOMYZiKb1lzHAzn0AiFjh0P96XJVAqFrYtcaY1FP4xQ="
+    ];
+  };
+}
+    
